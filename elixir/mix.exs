66e2b94aef02fabb7ea90a7b86dd6b9209c00640
Jordan <jordan@iharc.ca>
2026-09-09 12:55:38 -0400

    chore: refresh release dependencies

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -70,17 +70,17 @@ defmodule SymphonyElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bandit, "~> 1.8"},
+      {:bandit, "~> 1.12"},
       {:floki, ">= 0.30.0", only: :test},
       {:lazy_html, ">= 0.1.0", only: :test},
-      {:phoenix, "~> 1.8.0"},
+      {:phoenix, "~> 1.8.13"},
       {:phoenix_html, "~> 4.2"},
-      {:phoenix_live_view, "~> 1.1.0"},
-      {:req, "~> 0.5"},
-      {:jason, "~> 1.4"},
+      {:phoenix_live_view, "~> 1.1.33"},
+      {:req, "~> 0.7.4"},
+      {:jason, "~> 1.4.5"},
       {:yaml_elixir, "~> 2.12"},
-      {:solid, "~> 1.2"},
-      {:ecto, "~> 3.13"},
+      {:solid, "~> 1.3"},
+      {:ecto, "~> 3.14"},
       {:burrito, "~> 1.5", only: :prod, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
